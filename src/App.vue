eae1d2c783cc1d34c7fe8e6748e010026f468de3
Nathan Anthony <nsanthony95@gmail.com>
2022-04-02 23:38:47 +0200

    Added components to vue to render on dom

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,42 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HeaderNavigation from "./components/HeaderNavigation.vue";
+import TripSlider from "./components/TripSlider.vue";
+import TabWrapper from "./components/TabWrapper.vue";
+import TabItems from "./components/TabItems.vue";
+import PlacesVist from "./components/PlacesVist.vue";
+import FeaturedPlaces from "./components/FeaturedPlaces.vue";
+import PlacesList from "./components/PlacesList.vue";
+import FooterNavigation from "./components/FooterNavigation.vue";
+import TripSliderItems from "./components/TripSliderItems.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header>
+    <HeaderNavigation />
   </header>
 
   <main>
-    <TheWelcome />
+    <TabWrapper>
+      <TabItems title="DISCOVER">
+        <TripSlider />
+        <FeaturedPlaces />
+        <PlacesVist />
+        <PlacesList />
+      </TabItems>
+      <TabItems title="ACTIVITIES">Activities content</TabItems>
+    </TabWrapper>
   </main>
+  <footer>
+<FooterNavigation />
+  </footer>
+
 </template>
 
-<style>
-@import './assets/base.css';
-
+<style lang="scss">
 #app {
-  max-width: 1280px;
+  max-width: 425px;
   margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  background: $appBg;
 }
 </style>
