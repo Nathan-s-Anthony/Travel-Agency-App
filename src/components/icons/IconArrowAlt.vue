cf66a22658eeccdd3ef679b27e30d3405bb01b5d
Nathan Anthony <nsanthony95@gmail.com>
2022-04-03 23:35:15 +0200

    added new icons

---
+++ src/components/icons/IconArrowAlt.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    version="1.2"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 7 11"
+    width="7"
+    height="11"
+    fill="#fff"
+    fill-opacity="0.5"
+  >
+    <defs>
+      <clipPath clipPathUnits="userSpaceOnUse" id="cp1">
+        <path d="m-327-454h375v2171h-375z" />
+      </clipPath>
+    </defs>
+   
+    <g clip-path="url(#cp1)">
+      <path
+        class="a"
+        d="m5.9 4.7c0.4 0.4 0.4 1 0 1.4l-3.7 3.8c-0.4 0.4-1.1 0.4-1.4 0-0.4-0.4-0.4-1.1 0-1.4l3.7-3.8c0.4-0.4 1-0.4 1.4 0zm-3.7-3.8l3.7 3.8c0.4 0.4 0.4 1 0 1.4-0.4 0.4-1 0.4-1.4 0l-3.7-3.8c-0.4-0.3-0.4-1 0-1.4 0.3-0.4 1-0.4 1.4 0z"
+      />
+    </g>
+  </svg>
+</template>
---
+++ src/components/icons/IconComment.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    version="1.2"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 15 14"
+    width="15"
+    height="14"
+     fill=" #aaa"
+  >
+    <defs>
+      <clipPath clipPathUnits="userSpaceOnUse" id="cp1">
+        <path d="m-296-2024h375v2171h-375z" />
+      </clipPath>
+    </defs>
+  
+    <g clip-path="url(#cp1)">
+      <path
+        fill-rule="evenodd"
+        class="a"
+        d="m1.5 0.8h11.3c0.8 0 1.4 0.6 1.4 1.3v7.3c0 0.7-0.6 1.3-1.4 1.3h-8.3l-2.5 2.5c-0.1 0.3-0.4 0.5-0.8 0.5h-0.2c-0.4 0-0.8-0.3-0.8-0.8v-0.2c0 0 0-3.3 0-3.3v-7.3c0-0.7 0.6-1.3 1.3-1.3zm0.5 9.9l1.5-1.8h8.9v-6.3h-10.4z"
+      />
+    </g>
+  </svg>
+</template>
