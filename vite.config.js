839ffacb79e6b31094eca9d644832e1fb70290f5
Nathan Anthony <nsanthony95@gmail.com>
2022-04-03 13:54:13 +0200

    updated configs

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { fileURLToPath, URL } from 'url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/layout/base.scss";@import "@/assets/scss/colors/colors.scss";@import "@/assets/scss/font/typography.scss";`,
+      },
+    },
+  },
+});
