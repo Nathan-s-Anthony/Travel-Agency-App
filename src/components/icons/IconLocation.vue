4844073f0a0589b522c8a38fb55ccbc28447e8bc
Nathan Anthony <nsanthony95@gmail.com>
2022-04-03 22:16:30 +0200

    new icon added

---
+++ src/components/icons/IconLocation.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    version="1.2"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 13 17"
+    width="13"
+    height="17"
+      fill="#aaa"
+  >
+    <defs>
+      <clipPath clipPathUnits="userSpaceOnUse" id="cp1">
+        <path d="m-143-2023h375v2171h-375z" />
+      </clipPath>
+    </defs>
+ 
+    <g clip-path="url(#cp1)">
+      <path
+        fill-rule="evenodd"
+        class="a"
+        d="m12.9 6.3c0 4.5-5.5 9.6-5.7 9.9q-0.3 0.2-0.6 0.2-0.3 0-0.6-0.2c-0.2-0.3-5.7-5.4-5.7-9.9 0-4.6 3.4-6.2 6.3-6.2 2.9 0 6.3 1.6 6.3 6.2zm-1.6 0c0-4.3-3.6-4.6-4.7-4.6-1.1 0-4.7 0.3-4.7 4.6 0 3 3.3 6.8 4.7 8.2 1.4-1.4 4.7-5.2 4.7-8.2zm-4.7-2c1.1 0 2 0.8 2 1.9 0 1-0.9 1.9-2 1.9-1.1 0-2-0.9-2-1.9 0-1.1 0.9-1.9 2-1.9z"
+      />
+    </g>
+  </svg>
+</template>
