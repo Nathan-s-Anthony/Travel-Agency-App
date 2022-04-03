ecc460405fbfe01fd00bd588ccd4b0811809ccfc
Nathan Anthony <nsanthony95@gmail.com>
2022-04-03 12:11:24 +0200

    connected to api to consume api endpoint

--- src/components/FeaturedPlacesItems.vue
+++ src/components/FeaturedPlacesItems.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="featured-tiles">
-      <slot></slot>
+  <div class="featured-tiles-container">
+      <div class="featured-tile-items" v-for="featured in featured" :key="featured.title">
+    
+    <h5>{{ featured.title }} {{ featured.price }}</h5>
+    <p>{{ featured.title }}</p>
   </div>
+  </div>
+ 
 </template>
+<style  lang="scss">
+@import "../assets/scss/components/featuredplaces.scss";
+</style>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      featured: [],
+    };
+  },
+  mounted() {
+ 
+    axios
+      .get("https://www.afrihost.com/resources/fedev/places.json")
+      .then((response) => {
+        this.featured = response.data.featured;
+      });
+  },
+};
+
+</script>
