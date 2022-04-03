4010f2e2244fb84f162c5b8f1eeb6d6e7d2f4050
Nathan Anthony <nsanthony95@gmail.com>
2022-04-03 18:23:01 +0200

    added a catch for the api call

--- src/components/FeaturedPlacesItems.vue
+++ src/components/FeaturedPlacesItems.vue
@@ -16,10 +16,18 @@
         <p>${{ featured.price }}</p>
         <span></span>
         <p>{{ featured.location }}</p>
-       
       </div>
-      <div class="test">
-        {{ featured.meta.weather.temperature }}
+      <div class="featured-tile-additional">
+        <div class="featured-tile-weather">
+          <IconSunnyAlt />
+          <p>{{ featured.meta.weather.conditions }}</p>
+          <p>{{ featured.meta.weather.temperature }}</p>
+        </div>
+        <div class="featured-tile-likes">
+          <div>
+            <IconLove /> <sup>{{ featured.meta.likes }}K</sup>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,6 +38,7 @@
 <script>
 import IconMore from "./icons/IconMore.vue";
 import IconSunny from "./icons/IconSunny.vue";
+import IconSunnyAlt from "./icons/IconSunnyAlt.vue";
 import IconSaved from "./icons/IconSaved.vue";
 import IconLove from "./icons/IconLove.vue";
 import axios from "axios";
@@ -42,12 +51,16 @@ export default {
   },
   mounted() {
     //fetching the api data from the server
+
     axios
       .get("https://www.afrihost.com/resources/fedev/places.json")
       .then((response) => {
         this.featured = response.data.featured;
+      })
+      .catch((error) => {
+        console.log(error);
       });
   },
-  components: { IconMore, IconSunny, IconSaved, IconLove },
+  components: { IconMore, IconSunny, IconSaved, IconLove, IconSunnyAlt },
 };
 </script>
